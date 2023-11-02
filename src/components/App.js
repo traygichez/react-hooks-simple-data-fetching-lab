fcccc6c46c81ff361f0b9e8e3b693d8500b80faf
tray <onesimus.githua@student.moringaschool.com>
2023-11-02 19:22:52 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        setDogImage(data.message);
+      });
+  }, []);
+
+  
+  
+  return (
+    <div>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+
+
+export default App;
